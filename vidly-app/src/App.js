0125f5492f2bd9fb75f368973b595c9f7d86f258
cadolphe <christopher.adolphe@gmail.com>
2021-12-09 07:59:53 +0400

    Setting up course project Vidly-app

---
+++ vidly-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="col-lg-8 mx-auto p-3 py-md-5">
+      <main>
+        <h1>Vidly App</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
