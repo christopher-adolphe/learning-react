dbbdbf4369e53a3e1dee8c583d6420a5dc67c4f9
cadolphe <christopher.adolphe@gmail.com>
2021-12-09 09:20:50 +0400

    Understanding jsx, rendering list, conditional rendering, handling events and updating state of a component

--- vidly-app/src/App.js
+++ vidly-app/src/App.js
@@ -1,10 +1,11 @@
+import Movies from './components/movies';
 import './App.css';
 
 function App() {
   return (
     <div className="col-lg-8 mx-auto p-3 py-md-5">
       <main>
-        <h1>Vidly App</h1>
+        <Movies />
       </main>
     </div>
   );
---
+++ vidly-app/src/components/movies.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { getMovies, deleteMovie } from '../services/fakeMovieService';
+
+class Movies extends Component {
+  state = {
+    movies: getMovies()
+  }
+
+  render() {
+    return this.getMovies();
+  }
+
+  getMovies() {
+    const { movies } = this.state;
+
+    return movies.length
+      ? <React.Fragment>
+        <p>Showing { `${movies.length} ${movies.length > 1 ? `movies` : `movie`} ` } in the database</p>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {
+              movies.map(movie => (
+                <tr key={ movie._id }>
+                  <th scope="row">{ movie.title }</th>
+                  <td>{ movie.genre.name }</td>
+                  <td>{ movie.numberInStock }</td>
+                  <td>{ movie.dailyRentalRate }</td>
+                  <td><button className="btn btn-danger btn-sm" onClick={ () => this.handleDeleteMovie(movie._id) }>Delete</button></td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+        </React.Fragment> 
+    : <p>Sorry, there are no movies in the database.</p>
+  }
+
+  handleDeleteMovie = (id) => {
+    const movies = this.state.movies.filter(movie => movie._id !== id);
+
+    deleteMovie(id);
+
+    this.setState({ movies })
+  }
+}
+ 
+export default Movies;
