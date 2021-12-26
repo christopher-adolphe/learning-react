4e5d8eff001374005e96ece4001faf6d9b3d25c4
cadolphe <christopher.adolphe@gmail.com>
2021-12-26 09:25:54 +0400

    Added basic CRUD operations to http-app project

--- http-app/src/App.js
+++ http-app/src/App.js
@@ -1,27 +1,95 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import "./App.css";
+
+const apiEndpoint = 'https://jsonplaceholder.typicode.com/posts';
 
 class App extends Component {
   state = {
     posts: []
   };
 
-  handleAdd = () => {
-    console.log("Add");
+  async componentDidMount() {
+    try {
+      const { data: posts } = await axios.get(apiEndpoint);
+
+      this.setState({ posts });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  handleAdd = async () => {
+    const newPost = { title: 'New post title', body: 'New posts content' };
+
+    try {
+      const { data: post } = await axios.post(apiEndpoint, newPost);
+      const posts = [post, ...this.state.posts];
+      
+      this.setState({ posts });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  handleUpdate = post => {
-    console.log("Update", post);
+  handleUpdate = async post => {
+    post.title += ' updated!';
+
+
+    try {
+      // With the `put()` method we need to pass the entire object we want to update for a given resource
+      await axios.put(`${apiEndpoint}/${post.id}`, post);
+
+      // With the `patch()` method we can specify the properties we want to update for a given resource
+      // const { data: post } = await axios.patch(`${apiEndpoint}/${post.id}`, { title: post.title });
+
+      const posts = [ ...this.state.posts ];
+      const index = posts.indexOf(post);
+
+      posts[index] = { ...post };
+      this.setState({ posts });
+;    } catch (error) {
+      console.log(error);
+    }
   };
 
-  handleDelete = post => {
-    console.log("Delete", post);
+  handleDelete = async post => {
+    // Pessimistic Update
+    // try {
+    //   await axios.delete(`${apiEndpoint}/${post.id}`);
+
+    //   // const posts = [ ...this.state.posts ];
+    //   // const index = posts.indexOf(post);
+
+    //   // posts.splice(index, 1);
+
+    //   const posts = this.state.posts.filter(p => p.id !== post.id);
+
+    //   this.setState({ posts });
+    // } catch (error) {
+    //   console.log(error);
+    // }
+
+    // Optimistic Update
+    const initialPostsState = this.state.posts;
+    const posts = this.state.posts.filter(p => p.id !== post.id);
+
+    this.setState({ posts });
+
+    try {
+      await axios.delete(`${apiEndpoint}/${post.id}`);
+      // throw new Error('Something went wrong while deleting post!');
+    } catch (error) {
+      // console.log(error);
+      alert(error)
+      this.setState({ posts: initialPostsState });
+    }
   };
 
   render() {
     return (
       <React.Fragment>
-        <button className="btn btn-primary" onClick={this.handleAdd}>
+        <button className="btn btn-primary mb-4" onClick={this.handleAdd}>
           Add
         </button>
         <table className="table">
